061dcc542f7d184b1c2c6782f144665db9f69915
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-07-02 17:54:03 -0400

    Added module exports

--- src/pible/__init__.py
+++ src/pible/__init__.py
@@ -1,3 +1,12 @@
 """A Python library for working with Bible books and verses."""
 
+from .pible import Bible, BibleBook, BibleChapter, BibleVerse
+
 __version__ = "0.1"
+
+__all__ = (
+    "Bible",
+    "BibleBook",
+    "BibleChapter",
+    "BibleVerse"
+)
