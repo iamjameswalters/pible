d98bb18250b83673e4eceb75340d9b9a16bd8e8d
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-07-06 16:19:59 -0400

    Clean up esv code

--- src/pible/translations/esv/esv.py
+++ src/pible/translations/esv/esv.py
@@ -1,4 +1,5 @@
-import re, urllib.request
+import re
+import urllib.request
 
 try:
     import ujson as json
@@ -8,22 +9,15 @@ except ImportError:
 API_ENDPOINT = "https://api.esv.org/v3/passage/text/"
 
 def request_from_api(verse):
-    url = f"{API_ENDPOINT}?q={verse._book_title}+{verse._chapter_number}:{verse._verse_number}&include-passage-references=false&include-verse-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-selahs=false"
+    url = f"{API_ENDPOINT}?q={verse._book_title}+{verse._chapter_number}:{verse.verse_number}&include-passage-references=false&include-verse-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-selahs=false"
     request = urllib.request.Request(url)
     request.add_header("Authorization", f"Token {verse.api_key}")
-    response = urllib.request.urlopen(request)
-    if not 200 <= response.status < 300:
-        raise HttpError(response.status)
-    return response
-
-def parse_response_data(response):
-    data = json.loads(response.read())
-    verse = data["passages"][0]
-    pattern = "(.*?)\s\(ESV\)"
-    match = re.search(pattern, verse)
-    return match[1]
+    response = urllib.request.urlopen(request, timeout=10)
+    return json.loads(response.read())
 
 
 def get_verse_text(verse):
-    api_response = request_from_api(verse)
-    return parse_response_data(api_response)
+    data = request_from_api(verse)
+    if data["query"] != verse.address:
+        raise ValueError(f"{verse._book_title} chapter {verse._chapter_number} does not contain verse {verse.verse_number}.")
+    return data["passages"][0]
